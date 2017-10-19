3d2d99a75d52889c1f46c5c7f3ef0319ba8810a2
Maarten van Gompel <proycon@anaproy.nl>
2017-10-19 13:00:20 +0200

    New approach, more evaluation-centric and using only BabelFy.  Implemented buffer to maximize request/babelcoin.

--- babelente/babelente.py
+++ babelente/babelente.py
@@ -14,15 +14,55 @@ import pickle
 from babelpy.babelfy import BabelfyClient
 
 
-def findentities(text, lang, apikey):
+def gettextchunks(lines, maxchunksize=4096):
+    """Partition lines into text chunks"""
+    offsetmap = {} #(begin, end) tuple for the line
+    firstlinenr = 0
+    lastlinenr = 0
+    text = ""
+    for i, line in enumerate(lines):
+        if len(text) + len(line) + 1 >= maxchunksize:
+            #yield the current chunk
+            if text:
+                yield text, firstlinenr, lastlinenr, offsetmap
+
+            #start a new chunk
+            offsetmap = {i: (0,len(line))}
+            text = line
+            firstlinenr = i
+            lastlinenr = i
+        else:
+            if text:
+                begin = len(text) + 1
+                text = "\n" + line
+            else:
+                begin = 0
+                text = line
+            lastlinenr = i
+            offsetmap[i] = (begin, begin+len(line))
+
+    #don't forget last one:
+    if text:
+        yield text, firstlinenr, lastlinenr, offsetmap
+
+def resolveoffset(offsetmap, offset):
+    """Convert a relative character offset in a chunk to an absolute line number"""
+    for linenr, (begin, end) in offsetmap.items():
+        if offset >= begin and offset < end:
+            return linenr
+    raise ValueError("Unable to resolve offset " + str(offset))
+
+def findentities(lines, lang, apikey):
+    """Find entities using BabelFy given a set of input lines"""
     babelclient = BabelfyClient(apikey, {'lang': lang.upper()})
-    babelclient.babelfy(text)
-    for entity in babelclient.entities:
-        yield entity
+    for text, firstlinenr, lastlinenr, offsetmap in gettextchunks(lines, maxchunksize=4096):
+        babelclient.babelfy(text)
+        for entity in babelclient.entities:
+            entity['linenr'] = resolveoffset(entity['start'])
+            yield entity
 
 def findtranslations(synset_id, lang, apikey, cache=None):
-    """translate entity to target language"""
-
+    """Translate entity to target language (current not used!)"""
     if cache is not None:
         if synset_id in cache and lang in cache[synset_id]:
             for lemma in cache[synset_id][lang]:
@@ -46,13 +86,13 @@ def findtranslations(synset_id, lang, apikey, cache=None):
             cache[synset_id][lang].add(sense['lemma'])
 
 def main():
-    parser = argparse.ArgumentParser(description="BabelEnte: Entity Extractor and Translator using BabelFy and Babelnet.org", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description="BabelEnte: Entity extractioN, Translation and Evaluation using BabelFy and Babelnet.org", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-k','--apikey', type=str,help="Babelnet API key", action='store',default="",required=True)
     parser.add_argument('-f','--sourcelang', type=str,help="Source language code", action='store',default="",required=True)
     parser.add_argument('-t','--targetlang', type=str,help="Target language code", action='store',default="",required=True)
-    parser.add_argument('-i','--input', type=str,help="Input sentences (plain text, one per line, utf-8)", action='store',default="",required=True)
+    parser.add_argument('-S','--source', type=str,help="Source sentences (plain text, one per line, utf-8)", action='store',default="",required=True)
+    parser.add_argument('-T','--target', type=str,help="Target sentences (plain text, one per line, utf-8)", action='store',default="",required=True)
     parser.add_argument('-c','--cache', type=str,help="Cache file", action='store',required=False)
-    parser.add_argument('--eval', type=str,help="Evaluate against specified file with output sentences", action='store',default="",required=False)
     args = parser.parse_args()
 
     if args.cache:
@@ -63,17 +103,18 @@ def main():
             print("Cache does not exist yet, creating new one",file=sys.stderr)
             cache = {}
 
-    with open(args.input, 'r',encoding='utf-8') as f:
-        text = f.read()
+    with open(args.source, 'r',encoding='utf-8') as f:
+        sourcelines = [ l.strip() for l in f.readlines() ]
+    with open(args.target, 'r',encoding='utf-8') as f:
+        targetlines = [ l.strip() for l in f.readlines() ]
 
-    entities = []
-    for entity in findentities(text, args.sourcelang,args.apikey):
-        if entity['isEntity'] and 'babelSynsetID' in entity: #sanity check
-            entity['translations'] = set()
-            for translatedlemma in findtranslations(entity['babelSynsetID'], args.targetlang.upper(), args.apikey, cache):
-                entity['translations'].add(translatedlemma.replace('_',' '))
-                entities.append(entity)
-    print(json.dumps({'entities':entities}, indent=4,ensure_ascii=False))
+    if len(sourcelines) != len(targetlines):
+        print("ERROR: Expected the same number of line in source and target files, but got " + str(len(sourcelines)) + " vs " + str(len(targetlines)) ,file=sys.stderr)
+        sys.exit(2)
+
+    sourceentities = [ (entity, linenr) for  entity, linenr in findentities(sourcelines, args.sourcelang, args.apikey) if entity['isEntity'] and 'babelSynsetID' in entity ] #with sanity check
+    targetentities = [ (entity, linenr) for  entity, linenr in findentities(targetlines, args.targetlang, args.apikey) if entity['isEntity'] and 'babelSynsetID' in entity ] #with sanity check
+    print(json.dumps({'sourceentities':sourceentities, 'targetentities': targetentities}, indent=4,ensure_ascii=False))
     if args.cache:
         with open(args.cache,'wb') as f:
             pickle.dump(cache, f)
